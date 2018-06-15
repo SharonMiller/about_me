7ac45511175498ad07c4d332f41ce099aea9f167
Sharon Miller <sharon@sharonmillercreative.com>
2018-06-15 08:07:37 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # about_me
 A site about Sharon
+
+The about me was game was coded by
+Driver: Alex
+Navigator: Sharon
--- js/appdemo.js
+++ js/appdemo.js
@@ -1,34 +1,71 @@
 'use strict';
 
-alert('Welcome to my site, lets play a guessing game about me');
+//day 4 demo:
+//
+// console.log('im working'); 
+// function syntax
+var clickButton= document.getElementById('clickMe');
+console.log(clickButton);
 
-// var userName= 'sharon';
+clickButton.addEventListener('click', function(){
+  console.log('you clicked me');
+});
 
-var moveGuess = prompt('how many times have I moved');
-var numGuess;
-
-for (numGuess = 0; numGuess < 3; numGuess++ ) {
-  console.log(numGuess + ' my counting guess - top of for loop');
-  console.log(moveGuess + ' I input this - top of for loop');
-  if (moveGuess > 32) {
-    console.log('greater than 32');
-    alert('Oops, that is too high');
-    moveGuess = prompt('try again');
-    console.log(moveGuess);
-  } else if (moveGuess < 32) {
-    console.log('less than 32');
-    alert('Oops, that is too low');
-    moveGuess = prompt ('try again');
-  } else if (moveGuess == 32) {
-    console.log('equal to 32');
-    alert('Nice job, you guessed it');
-    break;
-  }
-  if (numGuess === 2 ) {
-    alert('You are all out of guesses.');
-  }
-  console.log(numGuess + ' x\'s through loop');
+function functionName (parameter1, parameter2) {
+  return parameter1 + parameter2;
 }
+function calculateArea(width, height) {
+  var area = width * height;
+  return area;
+}
+console.log(calculateArea(8,10));
+// calculateArea(); //invoking the function
+
+function circleProperties (radius){
+  var diameter = radius * 2;
+  var area = radius * radius * Math.PI;
+
+  return [diameter, area];//array
+}
+console.log(circleProperties(4));
+
+var sharonName = (function() {
+  return 'sharon';
+})(); //this is an iffeeee immidiately invoked function expression
+
+// alert('Welcome to my site, lets play a guessing game about me');
+
+// // var userName= 'sharon';
+
+// var moveGuess = prompt('how many times have I moved');
+// var numGuess;
+
+// for (numGuess = 0; numGuess < 3; numGuess++ ) {
+//   console.log(numGuess + ' my counting guess - top of for loop');
+//   console.log(moveGuess + ' I input this - top of for loop');
+//   if (moveGuess > 32) {
+//     console.log('greater than 32');
+//     alert('Oops, that is too high'); //guess too high
+//     moveGuess = prompt('try again');
+//     console.log(moveGuess);
+//   } else if (moveGuess < 32) {
+//     console.log('less than 32');
+//     alert('Oops, that is too low');
+//     moveGuess = prompt ('try again');
+//   } else if (moveGuess == 32) {
+//     console.log('equal to 32');
+//     alert('Nice job, you guessed it');
+//     break;
+//   }
+//   if (numGuess === 2 ) {
+//     alert('You are all out of guesses.');
+//   }
+//   console.log(numGuess + ' x\'s through loop');
+
+//   else {
+//     alert ('Wrong anser, try again!');
+// } 
+// }
 //may need to move .toLowerCase()
 
 // // while(true) {
@@ -102,3 +139,8 @@ for (numGuess = 0; numGuess < 3; numGuess++ ) {
 //   alert('Sorry you are out of guesses, the answer is 32');
 // }
 // else if mov
+
+// do{
+//   states = prompt();
+// } while (states !== statesLivedIn );
+//   var i = 0; 
